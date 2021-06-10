d48520ed315391342f0ce0a2b05f15b4d41d2ece
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-06-10 12:55:59 +0300

    10.06.2021 Level 2 Database Encryption Added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -15,10 +16,13 @@ app.use(bodyParser.urlencoded({
 mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true ,useUnifiedTopology: true},{ useFindAndModify: false });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-}
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
